249a586c2b34b6ffff9e67ecc7e8e73aede7dbe0
b-bastian <bbrugger@schueler.hakzell.at>
2026-01-22 10:57:45 +0100

    removed console.logs from requirerole

--- middleware/role.js
+++ middleware/role.js
@@ -1,16 +1,10 @@
 export function requireRole(...allowedRoles) {
   return (req, res, next) => {
     if (!req.user) {
-      console.log("REQ.USER:", req.user);
-      console.log("ROLE TYPE:", typeof req.user?.role);
-
       return res.status(401).json({ error: "Nicht authentifiziert." });
     }
 
     if (!allowedRoles.includes(req.user.role)) {
-      console.log("REQ.USER:", req.user);
-      console.log("ROLE TYPE:", typeof req.user?.role);
-
       return res.status(403).json({ error: "Keine Berechtigung." });
     }
 
