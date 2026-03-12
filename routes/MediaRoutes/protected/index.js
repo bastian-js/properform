4444c78aa911f9ceefb6581265562a192297396b
b-bastian <bbrugger@schueler.hakzell.at>
2026-03-12 08:43:28 +0100

    added getexercisebyid route

--- routes/MediaRoutes/protected/index.js
+++ routes/MediaRoutes/protected/index.js
@@ -3,6 +3,7 @@ import uploadMediaRoutes from "./uploadMedia.js";
 import listMediaRoutes from "./listMedia.js";
 import deleteMediaRoutes from "./deleteMedia.js";
 import updateMediaRoutes from "./updateMedia.js";
+import getMediaById from "./getMediaById.js";
 
 const router = express.Router();
 
@@ -10,5 +11,6 @@ router.use(uploadMediaRoutes);
 router.use(listMediaRoutes);
 router.use(deleteMediaRoutes);
 router.use(updateMediaRoutes);
+router.use(getMediaById);
 
 export default router;
